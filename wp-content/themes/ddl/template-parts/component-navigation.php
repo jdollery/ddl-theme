af8f8dcdfaa592c7da0f17b44cdd9f8a8c14e459
jdollery <54100876+jdollery@users.noreply.github.com>
2021-07-06 17:27:26 +0100

    Fluid type mixin for headings

--- wp-content/themes/ddl/template-parts/component-navigation.php
+++ wp-content/themes/ddl/template-parts/component-navigation.php
@@ -2,9 +2,9 @@
   <h4 id="primaryNavigation" class="sr-only">Primary navigation</h4>
   <ul aria-labelledby="primaryNavigation">
     <?php wp_nav_menu( array (
-    'theme_location' => 'header-menu',
-    'container' => false,
-    'items_wrap' => '%3$s'
+      'theme_location' => 'header-menu',
+      'container' => false,
+      'items_wrap' => '%3$s'
     ) ); ?>
   </ul>
 </nav>
