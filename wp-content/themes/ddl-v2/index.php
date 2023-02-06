1964d7e0f743eecae81ee2d5df524485bc173a25
jdollery <54100876+jdollery@users.noreply.github.com>
2023-02-06 14:38:30 +0000

    Update index.php

--- wp-content/themes/ddl-v2/index.php
+++ wp-content/themes/ddl-v2/index.php
@@ -2,12 +2,22 @@
 
 <?php get_template_part('inc/banner'); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-<article class="block-p">
-  <?php the_content(); ?>
-</article>
-
-<?php endwhile; ?>
+<section class="archive block-p">
+  <ul class="archive__list">
+    <?php while ( have_posts() ) : the_post(); ?>
+    
+      <li class="archive__item">
+        <a class="archive__link" href="<?php echo get_permalink(); ?>">
+          <div class="archive__body">
+            <h3 class="archive__heading"><?php the_title(); ?></h3>
+            <div class="archive__summary"><?php echo strip_tags( the_excerpt() ); ?></div>
+          </div>
+          <span class="btn btn--black btn--block">Learn more</span>
+        </a>
+      </li>
+  
+    <?php endwhile; ?>
+  </ul>
+</section>
 
 <?php get_footer();?>
