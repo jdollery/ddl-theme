014be61f0b4c3c91832243f8eab52c372b7b59cd
jdollery <54100876+jdollery@users.noreply.github.com>
2023-02-17 15:19:38 +0000

    Updates after Enhance

--- wp-content/themes/ddl-v2/header.php
+++ wp-content/themes/ddl-v2/header.php
@@ -11,6 +11,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Dental Design - https://dental-design.marketing">
     <?php wp_head(); ?>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/img/site.webmanifest">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
--- wp-content/themes/gulpfile.js
+++ wp-content/themes/gulpfile.js
@@ -1,37 +1,27 @@
 // npm init -y
-// npm install gulp sass node-sass gulp-sass gulp-sourcemaps gulp-concat gulp-uglify --save-dev
+// npm i gulp sass node-sass gulp-sass gulp-sourcemaps gulp-clean-css --save-dev
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
 
 const paths = {
   styles: {
     src: './ddl-v2/assets/scss/**/*.scss',
     dest: './ddl-v2'
-  },
-  scripts: {
-    src: [
-      // './ddl-v2/assets/js/navigation.js',
-      // './ddl/assets/js/select2.js',
-      // './ddl-v2/assets/js/slick.js',
-      // './ddl/assets/js/wow.js',
-      // './ddl/assets/js/validate.js',
-      './ddl-v2/assets/js/script.js'
-    ],
-    dest: './ddl-v2'
   }
 };
 
-	
+
 function css() {
   return gulp
   .src(paths.styles.src)
   .pipe(sourcemaps.init())
-  .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+  // .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+  .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+  .pipe(cleanCSS({format: 'beautify'}))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest(paths.styles.dest))
 }
@@ -39,20 +29,8 @@ function css() {
 exports.css = css;
 
 
-function js() {
-  return gulp
-  .src(paths.scripts.src)
-  .pipe(concat('script.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(paths.scripts.dest))
-}
-
-exports.js = js;
-
-
 function watch() {
   gulp.watch(paths.styles.src, css);
-  gulp.watch(paths.scripts.src, js);
 }
 
 exports.watch = watch;
