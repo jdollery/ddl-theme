6eb71d4878795ad999017feb8bbcd13af2e0a281
jdollery <54100876+jdollery@users.noreply.github.com>
2023-03-28 22:47:46 +0100

    Gulp and nav update

--- wp-content/themes/gulpfile.js
+++ wp-content/themes/gulpfile.js
@@ -1,25 +1,50 @@
 // npm init -y
+// npm install gulp sass node-sass gulp-sass gulp-sourcemaps gulp-concat gulp-uglify --save-dev
 // npm i gulp sass node-sass gulp-sass gulp-sourcemaps gulp-clean-css --save-dev
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
+// const concat = require('gulp-concat');
+// const uglify = require('gulp-uglify');
 
 
 const paths = {
+
   styles: {
     src: './ddl-v2/assets/scss/**/*.scss',
     dest: './ddl-v2'
   }
+
+  // scripts: {
+  //   src: [
+  //     './ddl-v2/assets/js/navigation.js',
+  //     './ddl-v2/assets/js/select2.js',
+  //     './ddl-v2/assets/js/slick.js',
+  //     './ddl-v2/assets/js/wow.js',
+  //     './ddl-v2/assets/js/validate.js',
+  //     './ddl-v2/assets/js/script.js'
+  //   ],
+  //   dest: './ddl-v2'
+  // }
+
 };
 
+
+// function css() {
+//   return gulp
+//   .src(paths.styles.src)
+//   .pipe(sourcemaps.init())
+//   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+//   .pipe(sourcemaps.write('.'))
+//   .pipe(gulp.dest(paths.styles.dest))
+// }
 
 function css() {
   return gulp
   .src(paths.styles.src)
   .pipe(sourcemaps.init())
-  // .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
   .pipe(cleanCSS({format: 'beautify'}))
   .pipe(sourcemaps.write('.'))
@@ -29,8 +54,20 @@ function css() {
 exports.css = css;
 
 
+// function js() {
+//   return gulp
+//   .src(paths.scripts.src)
+//   .pipe(concat('script.js'))
+//   .pipe(uglify())
+//   .pipe(gulp.dest(paths.scripts.dest))
+// }
+
+// exports.js = js;
+
+
 function watch() {
   gulp.watch(paths.styles.src, css);
+  // gulp.watch(paths.scripts.src, js);
 }
 
 exports.watch = watch;
