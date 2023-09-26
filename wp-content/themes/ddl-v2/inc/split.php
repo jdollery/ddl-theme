16d1681e2a452ff98db324170844d362b7813bf7
jdollery <134281763+dental-design@users.noreply.github.com>
2023-09-26 17:02:43 +0100

    template changes

--- wp-content/themes/ddl-v2/inc/split.php
+++ wp-content/themes/ddl-v2/inc/split.php
@@ -7,7 +7,6 @@ if( have_rows('split_section') ){
   $split_hide = get_sub_field('split_hide');
   $split_bg = get_sub_field('split_bg');
   $split_location = get_sub_field('split_location');
-  $split_border = get_sub_field('split_border');
   $split_img = get_sub_field('split_img');
   $split_video = get_sub_field('split_video');
   $split_embed = get_sub_field('split_embed');
@@ -23,16 +22,10 @@ if( have_rows('split_section') ){
     <div class="split__row">
 
       <div class="split__one" >  
-        <div class="split__media<?php if($split_border == true) { ?> split__media--border<?php } ?>" >
+        <div class="split__media" >
 
           <?php if($split_video) { ?>
-            
-            <!-- <video src="<?php echo $split_video ?>" controls type="video/mp4" poster="<?php echo $split_poster ?>"> 
-              Sorry, your browser doesn’t support embedded videos.
-            </video> -->
-
             <img id="videoToggle" src="<?php echo $split_poster ?>" alt="<?php the_sub_field('title'); ?>" data-url="<?php echo $split_video ?>">
-
           <?php } elseif($split_embed) { ?>
             <?php echo $split_embed ?>
           <?php } else { ?>
@@ -42,7 +35,7 @@ if( have_rows('split_section') ){
         </div>
       </div>
 
-      <div class="split__two wow <?php if($split_location['value'] == 'left') { ?>fadeInRight<?php } elseif($split_location['value'] == 'right') {?>fadeInLeft<?php } ?>" data-wow-duration="1s" data-wow-delay="0.5s">
+      <div class="split__two">
         <?php echo $split_text ?>
       </div>
 
--- wp-content/themes/ddl-v2/inc/team.php
+++ wp-content/themes/ddl-v2/inc/team.php
@@ -1,104 +1,140 @@
 <section class="team">
   <div class="team__row">
+    <ul class="team__list">
 
-    <?php
-    
-    $loop = new WP_Query( array(
-      'post_type' => 'team', 
-      'posts_per_page' => '100',
-      'order'  => 'ASC', 
-      'orderby' => 'menu_order',
-    ) );
+      <?php
+      
+      $loop = new WP_Query( array(
+        'post_type' => 'team', 
+        'posts_per_page' => '100',
+        'order'  => 'ASC', 
+        'orderby' => 'menu_order',
+      ) );
 
-    while ( $loop->have_posts() ) : $loop->the_post(); ?>
+      while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-      <article class="member">
+        <li class="member">
 
-        <?php
+          <?php
 
-        $thumb_id = get_post_thumbnail_id( $post->ID );
-        $thumb_alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
-        $thumb_title = get_the_title($thumb_id);
-        $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
-        $thumb_url = $thumb_url_array[0];
+          $thumb_id = get_post_thumbnail_id( $post->ID );
+          $thumb_alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
+          $thumb_title = get_the_title($thumb_id);
+          $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+          $thumb_url = $thumb_url_array[0];
 
-        $member_job = get_field('member_job');
-        $member_gdc = get_field('member_gdc'); 
-        $member_creds = get_field('member_creds');
+          $member_job = get_field('member_job');
+          $member_gdc = get_field('member_gdc'); 
+          $member_creds = get_field('member_creds');
 
-        global $post;
-        $member_id = $post->post_name;
+          global $post;
+          $member_id = $post->post_name;
 
-        ?>
+          ?>
 
-        <div class="member__body">
-          <div class="cell-row cell-row-gutter">
-            <div class="cell cell-gutter span-12 pb-6 pb-sm-9">
-              <picture class="member__media" <?php if( get_the_content() ) { ?>data-open="<?php echo $member_id ?><?php } ?>" role="button" aria-pressed="false" aria-expanded="false" aria-controls="<?php echo $member_id ; ?>" >
-                <?php if ( has_post_thumbnail() ) { ?>
-                <img class="member__img" src="<?php echo $thumb_url ?>" alt="<?php echo $thumb_alt ?>">
-                <?php } else { ?>
-                <img class="member__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-profile.jpg" alt="<?php the_title(); ?>">
+          <div class="member__body">
+            <div class="member__row">
+              <div class="member__one">
+                <picture class="member__media">
+                  <?php if ( has_post_thumbnail() ) { ?>
+                    <img 
+                      class="member__img"
+                      src="<?php echo $thumb_url ?>" 
+                      alt="<?php echo $thumb_alt ?>"
+                      width="900"
+                      height="750"
+                      loading="lazy"
+                      decoding="async"
+                    >
+                  <?php } else { ?>
+                    <img 
+                      class="member__img" 
+                      src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-female.jpg" 
+                      alt="<?php the_title(); ?>"
+                      width="900"
+                      height="750"
+                      loading="lazy"
+                      decoding="async"
+                    >
+                  <?php } ?>
+                </picture>
+              </div>
+              <div class="member__two">
+                <h3 class="member__heading"><?php the_title(); ?></h3>
+                <?php if ( $member_job ) { ?>
+                  <p class="member__job"><?php echo $member_job ?></p>
                 <?php } ?>
-              </picture>
-            </div>
-            <div class="cell cell-gutter span-12 pb-6 pb-sm-9">
-              <h3 class="member__heading"><?php the_title(); ?></h3>
-              <?php if ( $member_job ) { ?>
-                <p class="member__job"><?php echo $member_job ?></p>
-              <?php } ?>
-              <?php if ( $member_gdc ) { ?>
-                <p class="member__gdc">GDC: <a href="https://olr.gdc-uk.org/SearchRegister/SearchResult?RegistrationNumber=<?php echo $member_gdc ?>" target="_blank"><?php echo $member_gdc ?></a></p>
-              <?php } ?>
-            </div>
-          </div>
-          <?php if( get_the_content() ) { ?>
-          <div class="cell-row cell-justify-center">
-            <button class="btn btn--accent w-100" data-open="<?php echo $member_id ; ?>" aria-pressed="false" aria-expanded="false" aria-controls="<?php echo $member_id ; ?>">Read profile</button>
-            <?php edit_post_link( __( 'Edit', 'textdomain' ), null, null, null, 'member__edit' ); ?>
-          </div>
-          <?php } ?>
-        </div>
-
-        <div class="slideout" id="<?php echo $member_id ; ?>" data-sideout="<?php echo $member_id ; ?>">
-          <div class="slideout__backdrop" data-close="<?php echo $member_id ; ?>">&nbsp;</div>
-          <div class="slideout__row">
-            <div class="slideout__body">
-              <!-- <button class="slideout__close" data-close="<?php echo $member_id ; ?>"><span class="icon icon--close"><svg role="img"><use xlink:href="#close" href="#close"></use></svg></span></button> -->
-              <div class="slideout__content">
-              <div class="slideout__media" data-open="<?php echo $member_id ; ?>">
-                <?php if ( has_post_thumbnail() ) { ?>
-                  <img class="slideout__img" src="<?php echo $thumb_url ?>" alt="<?php echo $thumb_alt ?>">
-                <?php } else { ?>
-                  <img class="slideout__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-profile.jpg" alt="<?php echo get_the_title(); ?>">
+                <?php if ( $member_gdc ) { ?>
+                  <p class="member__gdc">GDC: <a href="https://olr.gdc-uk.org/SearchRegister/SearchResult?RegistrationNumber=<?php echo $member_gdc ?>" target="_blank"><?php echo $member_gdc ?></a></p>
                 <?php } ?>
+              </div>
+            </div>
+            <?php if( get_the_content() ) { ?>
+            <div class="cell-row cell-justify-center">
+              <button class="btn btn--accent w-100" data-open="<?php echo $member_id ; ?>" aria-pressed="false" aria-expanded="false" aria-controls="<?php echo $member_id ; ?>">Read profile</button>
+              <?php edit_post_link( __( 'Edit', 'textdomain' ), null, null, null, 'member__edit' ); ?>
+            </div>
+            <?php } ?>
+          </div>
+
+          <div class="slideout" id="<?php echo $member_id ; ?>" data-sideout="<?php echo $member_id ; ?>">
+            <div class="slideout__backdrop" data-close="<?php echo $member_id ; ?>">&nbsp;</div>
+            <div class="slideout__row">
+              <div class="slideout__body">
+                <!-- <button class="slideout__close" data-close="<?php echo $member_id ; ?>"><span class="icon icon--close"><svg role="img"><use xlink:href="#close" href="#close"></use></svg></span></button> -->
+                <div class="slideout__content">
+                  <div class="slideout__media" data-open="<?php echo $member_id ; ?>">
+                    <picture>
+                      <?php if ( has_post_thumbnail() ) { ?>
+                        <img 
+                          class="member__img"
+                          src="<?php echo $thumb_url ?>" 
+                          alt="<?php echo $thumb_alt ?>"
+                          width="900"
+                          height="750"
+                          loading="lazy"
+                          decoding="async"
+                        >
+                      <?php } else { ?>
+                        <img 
+                          class="member__img" 
+                          src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder-female.webp" 
+                          alt="<?php the_title(); ?>"
+                          width="900"
+                          height="750"
+                          loading="lazy"
+                          decoding="async"
+                        >
+                      <?php } ?>
+                    </picture>
+                  </div>
+                  <h3 class="slideout__heading"><?php the_title(); ?></h3>
+                  <div class="slideout__meta">
+                    <?php if ( $member_job ) { ?>
+                      <h4 class="slideout__job"><?php echo $member_job ?></h4>
+                    <?php } ?>
+                    <?php if ( $member_creds ) { ?>
+                      <div class="slideout__creds"><?php echo $member_creds ?></div>
+                    <?php } ?>
+                    <?php if ( $member_gdc ) { ?>
+                      <p class="slideout__gdc">GDC Number: <a href="https://olr.gdc-uk.org/SearchRegister/SearchResult?RegistrationNumber=<?php echo $member_gdc ?>" target="_blank"><?php echo $member_gdc ?></a></p>
+                    <?php } ?>
+                  </div>
+                  <div class="slideout__profile"><?php the_content(); ?></div>
+                  <button class="btn btn--accent btn--block w-100" data-close="<?php echo $member_id ; ?>">Close profile</button>
                 </div>
-                <h3 class="slideout__heading"><?php the_title(); ?></h3>
-                <div class="slideout__meta">
-                  <?php if ( $member_job ) { ?>
-                    <h4 class="slideout__job"><?php echo $member_job ?></h4>
-                  <?php } ?>
-                  <?php if ( $member_creds ) { ?>
-                    <div class="slideout__creds"><?php echo $member_creds ?></div>
-                  <?php } ?>
-                  <?php if ( $member_gdc ) { ?>
-                    <p class="slideout__gdc">GDC Number: <a href="https://olr.gdc-uk.org/SearchRegister/SearchResult?RegistrationNumber=<?php echo $member_gdc ?>" target="_blank"><?php echo $member_gdc ?></a></p>
-                  <?php } ?>
-                </div>
-                <div class="slideout__profile"><?php the_content(); ?></div>
-                <button class="btn btn--accent btn--block w-100" data-close="<?php echo $member_id ; ?>">Close profile</button>
               </div>
             </div>
           </div>
-        </div>
 
-      </article>
+        </li>
 
-    <?php 
+      <?php 
 
-    endwhile; wp_reset_query(); 
-    
-    ?>
-    
+      endwhile; wp_reset_query(); 
+      
+      ?>
+
+    </ul>   
   </div>
 </section>
