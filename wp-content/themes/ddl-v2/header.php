4d232f8084e6ed66a8519959558ad7dc8ca4c362
jdollery <134281763+dental-design@users.noreply.github.com>
2023-12-11 10:54:22 +0000

    Moved wp_head below other meta

--- wp-content/themes/ddl-v2/header.php
+++ wp-content/themes/ddl-v2/header.php
@@ -10,7 +10,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Dental Design - https://dental-design.marketing">
-    <?php wp_head(); ?>
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon-16x16.png">
@@ -19,6 +18,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2Yj0cZfmd380OOF1JZQfSZEAxt_NbAtI" type="text/javascript"></script>
+    <?php wp_head(); ?>
 	</head>
 
   <body <?php body_class(); ?> >
