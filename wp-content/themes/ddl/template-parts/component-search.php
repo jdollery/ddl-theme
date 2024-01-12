b79707565232a197608b2b490ad07297f2a874a3
jdollery <134281763+dental-design@users.noreply.github.com>
2024-01-12 10:40:24 +0000

    Fix to spacing mixin duplication and ignore old theme

--- wp-content/themes/ddl/template-parts/component-search.php
+++
@@ -1,7 +0,0 @@
-<form class="search-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-  <div class="form__group">
-    <i class="fa fa-search"></i>
-    <input type="text" name="s" id="s" class="form-control" placeholder="I’m looking for…" aria-label="Search" autocomplete="off" required/>
-    <button type="submit" id="searchsubmit" class="button-primary">Search</button>
-  </div>
-</form>
