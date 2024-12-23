95b93b49eb5bee18fcc9a7e67149ec4d66c5a8af
jdollery <webdev@dental-design.co.uk>
2024-12-23 14:06:12 +0000

    Add dialog to footer

--- wp-content/plugins/ddl_chatbot/ddl-chatbot.php
+++ wp-content/plugins/ddl_chatbot/ddl-chatbot.php
@@ -152,10 +152,12 @@ function render_chatbot_onload_checkbox($args){
 function add_chatbot_dialog() {
 
   $chatbotScript = get_option('chatbot-iframe-script-textarea');
+  $chatbotOnload = get_option('chatbot-onload-checkbox');
+  $chatbotOnloadValue = ( !isset($chatbotOnload['chatbot-onload-checkbox'] ) ) ? '' : $chatbotOnload['chatbot-onload-checkbox'];
 
   echo '<div class="chat">
   <button class="chat__btn chat__btn--closed" id="chatToggle" type="button" aria-label="Open chat dialog" aria-haspopup="true" aria-controls="chatDialog" aria-expanded="false"><span class="chat__txt">Chat now</span><span class="chat__icon"></span></button>
-  <div class="chat__dialog chat__dialog--closed" id="chatDialog" role="dialog" aria-modal="true" aria-hidden="true"><iframe data-src="' . $chatbotScript . '" height="100%" width="100%" allow="clipboard-write *" style="border: none;"></iframe></div>
+  <div class="chat__dialog chat__dialog--closed' . ($chatbotOnloadValue == true ? ' chat__dialog--onload' : '') . '" id="chatDialog" role="dialog" aria-modal="true" aria-hidden="true"><iframe data-src="' . $chatbotScript . '" height="100%" width="100%" allow="clipboard-write *" style="border: none;"></iframe></div>
   </div>';
 }
 
